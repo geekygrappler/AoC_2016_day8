088b4eb37600c95c51d425de2943afa5e8186402
Andy <brownie3003@gmail.com>
2017-01-02 13:25:29 +0000

    Add run command

--- run.rb
+++ run.rb
@@ -1,16 +1,25 @@
-require_relative "lib/triangle.rb"
+require_relative "lib/screen.rb"
 require 'byebug'
 
-file = File.new("day3_input.txt")
-
-triangles = []
+file = File.new("day8_input.txt")
+screen = Screen.new()
 
 file.each_line do |l|
-    begin
-        triangles << Triangle.new(l.split(" ").map! { |i| i.to_i })
-    rescue
-        next
+    commands = l.split(" ")
+    command = commands.shift
+    if command == "rect"
+        size = commands.shift.split("x")
+        screen.add_rect(size.shift.to_i, size.shift.to_i)
+    elsif command == "rotate"
+        direction = commands.shift
+        arguments = commands.select { |x| x != "by" }
+        if direction == "row"
+            screen.rotate_row(arguments.shift.split("=").last.to_i, arguments.shift.to_i)
+        elsif direction == "column"
+            screen.rotate_column(arguments.shift.split("=").last.to_i, arguments.shift.to_i)
+        end
     end
 end
 
-puts triangles.length
+screen.print_screen
+puts screen.voltage
